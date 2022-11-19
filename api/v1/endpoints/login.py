e13ad22637ca1fa93f9e8ecbb78da5474633b392
mehdi <13610522>
2022-11-19 15:39:43 +0330

    test login

--- api/v1/endpoints/login.py
+++ api/v1/endpoints/login.py
@@ -1,5 +1,6 @@
 import grpc
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from schemas import LoginParam
 from service.client.customer import CustomerClient
 
@@ -9,8 +10,19 @@ router = APIRouter()
 async def login_request(param: LoginParam):
     try:
         result = CustomerClient.login_by_national_code(param.national_code)
-        return {"id": result.id, "message": result.message}
+        return JSONResponse(
+            content={"id": result.id, "message": result.message},
+            status_code=status.HTTP_200_OK
+        )
 
     except grpc.RpcError as e:
         if e.code() == grpc.StatusCode.NOT_FOUND:
-            return {"id": 400, "message": "کاربر یافت نشد"}
+            return JSONResponse(
+                content={"id": 400, "message": "کاربر یافت نشد"},
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
+
+        return JSONResponse(
+            content={"id": 400, "message": "کاربر یافت نشد"},
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
